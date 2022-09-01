88b71f760b61b68f3180451c587ed7b9b6c92a50
Alina Krasivskaya <linakrasivskaya@yandex.ru>
2022-09-01 16:59:37 +0300

    Апка

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const { errors, Joi, celebrate } = require('celebrate');
 const users = require('./routes/users');
 const cardRouter = require('./routes/cards');
-const { NOT_FOUND_CODE } = require('./errors/statusCode');
+const auth = require('./middlewares/auth');
 
 const { PORT = 3000 } = process.env;
+const NotFound = require('./errors/notFound');
+const INTERNAL_SERVER_ERROR = require('./errors/statusCode');
+const { login, createUser } = require('./controllers/users');
 
 const app = express();
 
@@ -13,23 +17,40 @@ app.use(bodyParser.json());
 
 mongoose.connect('mongodb://localhost:27017/mestodb', {
   useNewUrlParser: true,
+})
+
+app.post('/signin', celebrate({
+  body: Joi.object().keys({
+    email: Joi.string().required().email({ tlds: { allow: false } }),
+    password: Joi.string().required(),
+  }),
+}), login);
+app.post('/signup', celebrate({
+  body: Joi.object().keys({
+    name: Joi.string().min(2).max(30),
+    about: Joi.string().min(2).max(30),
+    avatar: Joi.string().regex(/https?:\/\/(\w{3}\.)?[1-9a-z\-.]{1,}\w\w(\/[1-90a-z.,_@%&?+=~/-]{1,}\/?)?#?/i),
+    email: Joi.string().required().email({ tlds: { allow: false } }),
+    password: Joi.string().required(),
+  }),
+}), createUser);
+
+app.use(auth);
+
+app.use('/users', users);
+app.use('/cards', cardRouter);
+app.use('*', (req, res, next) => {
+  next(new NotFound('Страница не найдена'));
 });
 
-app.use((req, res, next) => {
-  req.user = {
-    _id: '63011068079197be4613d766',
-  };
-  next();
-});
+app.use(errors());
 
-app.use('/', users);
-
-app.use('/', cardRouter);
-
-app.use('*', (req, res) => {
-  res.status(NOT_FOUND_CODE).send({
-    message: 'Страница не найдена',
-  });
+app.use((err, req, res, next) => {
+  if (err.statusCode) {
+    res.status(err.statusCode).send({ message: err.message });
+  } else {
+    res.status(INTERNAL_SERVER_ERROR).send({ message: 'Произошла ошибка' });
+  }
 });
 
 app.listen(PORT, () => {
