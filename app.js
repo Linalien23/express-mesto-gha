7fc79d2ad986977c826cf5fbc2936db598fd3f36
Alina Krasivskaya <linakrasivskaya@yandex.ru>
2022-09-10 18:35:01 +0300

    Логгер

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const { errors, Joi, celebrate } = require('celebrate');
 const users = require('./routes/users');
 const cardRouter = require('./routes/cards');
 const auth = require('./middlewares/auth');
+const { requestLogger, errorLogger } = require('./middlewares/logger');
 
 const { PORT = 3000 } = process.env;
 const NotFound = require('./errors/NotFound');
